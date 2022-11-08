808d85ce31277b08e369fdaf33c063373c0b106b
Cang Nguyen Duc <cang_nduc@live.com>
2022-11-09 02:39:44 +0700

    '2'

---
+++ 21810203/DAL/LoadingProducts.cs
@@ -0,0 +1,19 @@
+﻿namespace _21810203.DAL
+{
+    public class LoadingProducts
+    {
+        public string LoadingProductsFile(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+            string lines = sr.ReadToEnd();
+            sr.Close();
+            return lines;
+        }
+        public void savingProduct(string lines)
+        {
+            StreamWriter sw = new StreamWriter(lines);
+            sw.WriteLine();
+            sw.Close();
+        }
+    }
+}
---
+++ 21810203/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace _21810203.Entities
+{
+    public struct product
+    {
+        public int id;
+        public string name;
+        public double price;
+        public int quantity;
+        public DateOnly Expiration;
+        public bool isDeleted;
+    }
+    public struct Products
+    {
+        public string name;
+        public List<product> dsach;
+    }
+}
